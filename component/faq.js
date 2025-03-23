29e41a0062fa020846caa71a1d9127e88d339132
DMP92 <devin@spirradigital.com>
2025-03-23 03:03:54 -0400

    fix: refactor and move element queries into DomContentLoaded

--- component/faq.js
+++ component/faq.js
@@ -1,55 +1,57 @@
-// Toggles
-const faqToggles = document.querySelectorAll('[sd-faq-toggle]')
+// FAQ object
+const faq = {}
+// Update faq object
+window.addEventListener('DOMContentLoaded', () => {
+    faq.groupWrapper = document.querySelector('[sd-faq-element="group-wrapper"]')
+    faq.groups = document.querySelectorAll('[sd-faq-group]')
+    faq.toggles = document.querySelectorAll('[sd-faq-toggle]')
+    faq.desktopPosition = document.querySelector('[sd-faq-element="toggle-wrapper"]')
+    
+    // Init active variables
+    faq.active = {}
+    faq.active.toggle = document.querySelector('.faq-toggle.is-active')
+    faq.active.group = document.querySelector('.faq-group.is-active')
 
-// Wrapper
-const faqWrapper = document.querySelector('.faq-wrapper')
-
-// FAQ group
-const faqGroup = document.querySelectorAll('[sd-faq-group]')
-
-// FAQ positions
-const faqDesktopPosition = document.querySelector('.faq-toggle_wrapper')
-
-
-window.addEventListener('load', () => {
     updateFAQPositionOnDeviceWidth()
 })
 
+// Active FAQ toggle click event listeners
+window.addEventListener('DOMContentLoaded', () => {
+    faq.toggles.forEach(faqToggle => {
+        faqToggle.addEventListener('click', (e) => {
+            faq.active.toggle = faqToggle;
+            updateFAQToggles()
+            updateActiveFAQGroup()
+            updateFAQPositionOnDeviceWidth(faqToggle)
+        })
+    })
+})
+
+// Handle FAQ positioning on resize
 window.addEventListener('resize', () => {
     updateFAQPositionOnDeviceWidth()
 })
 
-function updateFAQPositionOnDeviceWidth(selectedFAQToggle) {
+function updateFAQPositionOnDeviceWidth() {
     if(window.innerWidth > 700) {
-        faqDesktopPosition.insertAdjacentElement("afterend", faqWrapper)
-    } else if(selectedFAQToggle) {
-        selectedFAQToggle.insertAdjacentElement("afterend", faqWrapper)
+        faq.desktopPosition.insertAdjacentElement("afterend", faq.groupWrapper)
     } else {
-        document.querySelector('.faq-toggle.is-active')
-            .insertAdjacentElement("afterend", faqWrapper)
+        faq.active.toggle.insertAdjacentElement("afterend", faq.groupWrapper)
     }
 }
 
-faqToggles.forEach(currentFAQToggle => {
-    currentFAQToggle.addEventListener('click', (e) => {
-        const faqToggleID = currentFAQToggle.getAttribute('sd-faq-toggle');
-        updateFAQToggles(currentFAQToggle)
-        updateActiveFAQGroup(faqToggleID)
-        updateFAQPositionOnDeviceWidth(currentFAQToggle)
-    })
-})
-
-function updateFAQToggles(clickedToggle) {
-    faqToggles.forEach(faqToggle => {
-        faqToggle === clickedToggle ?
-            faqToggle.classList.toggle('is-active') :
-            faqToggle.classList.remove('is-active')
+function updateFAQToggles() {
+    faq.toggles.forEach(toggle => {
+        toggle === faq.active.toggle ?
+            toggle.classList.toggle('is-active') :
+            toggle.classList.remove('is-active')
     })
 }
 
-function updateActiveFAQGroup(activeToggleID) {
-    faqGroup.forEach(group => {
-    if (group.getAttribute('sd-faq-group').includes(activeToggleID)) {
+function updateActiveFAQGroup() {
+    const activeToggleID = faq.active.toggle.getAttribute('sd-faq-toggle');
+    faq.groups.forEach(group => {
+    if (group.getAttribute('sd-faq-group') === activeToggleID) {
       group.classList.toggle('is-active')
     } else {
       group.classList.remove('is-active')
